5b78062ce232d28d6dfb573977a0e3fa237fb37d
ncottingham <nicholas.cottingham@puntersouthall.com>
2016-01-07 23:27:11 +0000

    added note on how to kill thread

--- app/src/main/java/com/bujok/ragstoriches/MainActivity.java
+++ app/src/main/java/com/bujok/ragstoriches/MainActivity.java
@@ -107,6 +107,8 @@ public class MainActivity extends AppCompatActivity {
             }
             else{
                 customHandler.postDelayed(this, 0);
+                //to kill thread
+                //customHandler.removeCallbacks(updateTimerThread);
             }
         }
     };
